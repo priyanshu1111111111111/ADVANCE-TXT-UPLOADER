b3bd2bbce443f4788ebd9cf8df2af77d6fb8c94a
priyanshu1111111111111 <Priyanshu909090901@gmail.com>
2025-05-07 18:10:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -830,7 +830,7 @@ async def upload(bot: Client, m: Message):
         await m.reply_text(e)
         await log_to_channel(bot, f"#ERROR\nError in /gaurav command\nError: {str(e)}\nBy: {m.from_user.id}")
     
-    await m.reply_text f"`📌𝗕𝗔𝗧𝗖𝗛 𝗡𝗔𝗠𝗘 : {b_name}`\n\n"
+   await m.reply_text f"`📌𝗕𝗔𝗧𝗖𝗛 𝗡𝗔𝗠𝗘 : {b_name}`\n\n"
         f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n"
         f"𝗟𝗘𝗔𝗥𝗡𝗜𝗡𝗚 𝗠𝗔𝗧𝗘𝗥𝗜𝗔𝗟 : {len(links)}\n"
         f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n"

1b404ac03481ab00d13e2dbdfcda49bd311c13dd
priyanshu1111111111111 <Priyanshu909090901@gmail.com>
2025-05-15 01:47:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -597,8 +597,8 @@ async def upload(bot: Client, m: Message):
 
     thumb = input6.text
     if thumb.startswith("http://") or thumb.startswith("https://"):
-        getstatusoutput(f"wget '{thumb}' -O 'thumb.png'")
-        thumb = "thumb.png"
+        getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
+        thumb = "thumb.jpg"
     else:
         thumb == "no"
     failed_count =0
